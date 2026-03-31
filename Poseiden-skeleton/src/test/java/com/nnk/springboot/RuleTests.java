56a95f3cf35b8e44097bf5c41fd657f6f9daa5f8
Rémi Granjon <remi.granjon@outlook.fr>
2026-03-31 13:09:05 +0200

    corrections diverses

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/RuleTests.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/RuleTests.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.when;
 
 import java.util.Optional;
 
-import org.apache.tomcat.util.digester.Rule;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
